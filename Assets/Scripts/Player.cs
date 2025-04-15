1c4240ff860a7a8d8edf47a6710541a1e5daf25f
Lotte <a0979093620@gmail.com>
2025-04-15 20:10:10 +0800

    Update_角色碰撞氣

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,22 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Player
+public class Player : MonoBehaviour
 {
-    
+    [Header("基本數值")]
+    [SerializeField,Range(0,10)]
+    private float moveSpeed = 3.5f;
+    [SerializeField,Range(0,15)]
+    private float jompForce = 10;
+    [SerializeField,Range(0,20)]
+    private float dashSpeed = 12;
+
+
+    private Animator ani;
+    private Rigidbody2D rig;
+
+    private void Awake()
+    {
+        ani = GetComponent<Animator>(); // 取得角色動畫元件
+        rig = GetComponent<Rigidbody2D>();// 取得角色鋼體(重力)元件
+    }
 }
